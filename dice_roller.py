74aee4b283e7886fd6de28360e7c88270e3865b8
kiranperaka <kiran.1905p6@gmail.com>
2020-09-24 10:40:02 +0530

    added flavours

--- dice_roller.py
+++ dice_roller.py
@@ -4,7 +4,12 @@ def main():
 	dice_sum = 0
 	for i in range(o,dict_rolls):
 		roll = random.randint(1,6)
-		print(f'You rolled a {roll}')
+                if roll == 1:
+                    print(f'you rolled a {roll}! Critical Fail')
+                elif roll == 6:
+                    print(f'you rolled a {roll}! Critical Success')
+                else:
+                    print(f'You rolled a {roll}')
 	print(f"you have rolled a total of {dice_sum})
 if __name__== "__main__":
   main()
